440227fb98b85d7268e072266f21bba60e5a143d
AndrewWaller <awalldrew2@gmail.com>
2018-04-09 20:09:10 -0700

    take home day-1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', 'particles.json', function(){
-    console.log('particles.json loaded...');
-  });
+particlesJS.load('particles-js', 'particles.json', function(){});
+
+  console.log ('I hope to learn more of the concepts of javascript and pick up on things that I did not learn from my first time in this class');
