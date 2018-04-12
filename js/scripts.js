87384ae571469db6cf58f416f5be2550a34a712b
AndrewWaller <awalldrew2@gmail.com>
2018-04-11 20:18:34 -0700

    strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 particlesJS.load('particles-js', 'particles.json', function(){
     console.log('particles.json loaded...');
   });
+
+var myName = "Andrew"+2+false;
+myName = new String(myName);
+
+console.log(typeof(myName));
+console.log(myName.toUpperCase());
