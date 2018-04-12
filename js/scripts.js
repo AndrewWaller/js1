8f75a414cc92a252175dbd74c7d66ba011250c29
AndrewWaller <awalldrew2@gmail.com>
2018-04-11 18:36:29 -0700

    quick fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 particlesJS.load('particles-js', 'particles.json', function(){});
 
-  console.log ('I hope to learn more of the concepts of javascript and pick up on things that I did not learn from my first time in this class');
+var myHistory = "born and raised in fresno, california";
+
+console.log (myHistory);
+
+console.log ('I hope to learn more of the concepts of javascript and pick up on things that I did not learn from my first time in this class');
