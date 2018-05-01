63e0ca6cb5de2cff59ad824e1aa1ac8b73d148de
AndrewWaller <awalldrew2@gmail.com>
2018-04-30 20:26:17 -0700

    day-7

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
 particlesJS.load('particles-js', 'particles.json', function(){});
->>>>>>> b428c46d117ca8c1aa536490bb43f161ba498d48
+
+function button() {
+  let title = prompt('Title?', "");
+  let h1 =  document.createElement('h1');
+  h1.setAttribute("class","center");
+  h1.innerHTML = title;
+  document.body.appendChild(h1);
+//Steps 1&2
+  let urDay = prompt("Please write a few sentences describing what you did today","");
+  let pTag = document.createElement('p');
+  pTag.setAttribute("class","justify");
+  pTag.innerHTML = urDay;
+  document.body.appendChild(pTag);
+  //Steps 3&4
+  let backgroundColor = prompt("What is your favorite color?","");
+  pTag.style.backgroundColor = backgroundColor;
+};
